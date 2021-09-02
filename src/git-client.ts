0c45bb474e1b86efaac61642f31f8706232a9f6b
SAMERkab <samerkab2001@outlook.de>
2021-09-02 20:19:34 +0200

    Convert class to creation method, add new cmds

--- src/git-client.ts
+++ src/git-client.ts
@@ -1,47 +1,56 @@
 import { prepareCommands } from './prepare-commands';
-import { Logger } from "./autoupdater";
+import { logger } from "./logger";
 
-const GIT_COMMANDS = {
+const GIT_COMMAND_DEFINITIONS = {
+  init: (directory: string) => ({ args: ["init -y"], cwd: directory }),
+  rootDirectory: (anySubdirectory: string) => ({args: ["rev-parse", "--show-toplevel"], cwd: anySubdirectory}),
   checkoutBranch: (branch: string) => ["checkout", branch],
   createBranch: (branch: string) => ["branch", branch],
-  deleteBranch: (branch: string) => ["branch", "-d", branch],
+  deleteLocalBranch: (branch: string) => ["branch", "-D", branch],
+  deleteRemoteBranch: (repository: string, branch: string) => ["push", "-d", repository, branch],
   branchExists: (branch: string) => ["show-ref", "--verify", "--quiet", `refs/heads/${branch}`],
   stageAndCommit: (message: string) => ["commit", "-am", message],
-  push: (repository = "", branch = "") => ["push", repository, branch]
+  push: (repository="", branch="") => ["push", repository, branch]
 } as const;
 
-export class GitClient {
-  private readonly logger: Logger;
-  private readonly git = prepareCommands("git", GIT_COMMANDS);
+export function initGitDirectory(directory: string) {
+  const initCommand = prepareCommands("git", { cmd: GIT_COMMAND_DEFINITIONS.init }).cmd;
+  initCommand(directory);
+}
 
-  constructor(logger: Logger = console) {
-    this.logger = logger;
+export function getGitRootDirectory(anySubdirectory=process.cwd()) {
+  const getRootDirectoryCommand = prepareCommands("git", {cmd: GIT_COMMAND_DEFINITIONS.rootDirectory}).cmd;
+  try {
+    return getRootDirectoryCommand(anySubdirectory);
+  } catch (error) {
+    return null;
   }
+}
 
-  createAndCheckoutBranch(branch: string) {
-    this.git.createBranch(branch);
-    this.git.checkoutBranch(branch);
+export function getGitClient(directory: string) {
+  const rootDirectory = getGitRootDirectory(directory);
+  if (rootDirectory === null) {
+    logger.error(`Directory "${directory}" is neither a git repository nor a subdirectory of a git repository.`);
+    return null;
   }
+  const git = prepareCommands("git", GIT_COMMAND_DEFINITIONS, rootDirectory);
+  return {
 
-  branchExists(branch: string): boolean {
-    try {
-      this.git.branchExists(branch);
-      return true;
-    } catch (error) {
-      return false;
+    ...git,
+
+    branchExists(branch: string): boolean {
+      try {
+        git.branchExists(branch);
+        return true;
+      } catch (error) {
+        return false;
+      }
+    },
+
+    addAndCommitChanges(message: string, branch: string) {
+      git.checkoutBranch(branch);
+      git.stageAndCommit(message);
     }
-  }
 
-  deleteBranch(branch: string) {
-    this.git.deleteBranch(branch);
-  }
-
-  addAndCommitChanges(message: string, branch: string) {
-    this.git.checkoutBranch(branch);
-    this.git.stageAndCommit(message);
-  }
-
-  push(repository: string, branch: string) {
-    this.git.push(repository, branch);
-  }
+  };
 }
