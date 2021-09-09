eeb6223520290294bd13df3be1a4576de6002abf
SAMERkab <samerkab2001@outlook.de>
2021-09-09 11:22:29 +0200

    Use cwd if no project root argument is present

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,8 @@
 import { Autoupdater } from "./autoupdater";
 
 try {
-  const app: Autoupdater = new Autoupdater(process.argv[2], process.argv.slice(3));
+  const projectRoot = process.argv[2] ?? process.cwd();
+  const app: Autoupdater = new Autoupdater(projectRoot, process.argv.slice(3));
   app.start();
 } catch (error) {
   console.error(error);
