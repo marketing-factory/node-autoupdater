ba761a45d7b73d3754a87958184bff5ee834f97f
SAMERkab <samerkab2001@outlook.de>
2021-09-22 15:46:08 +0200

    Make autoupdater fail on error

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,5 @@
 import { Autoupdater } from "./autoupdater";
 
-try {
-  const projectRoot = process.argv[2] ?? process.cwd();
-  const app: Autoupdater = new Autoupdater(projectRoot, process.argv.slice(3));
-  app.start();
-} catch (error) {
-  console.error(error);
-}
+const projectRoot = process.argv[2] ?? process.cwd();
+const app: Autoupdater = new Autoupdater(projectRoot, process.argv.slice(3));
+app.start();
