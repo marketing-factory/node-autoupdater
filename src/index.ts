f358b335eaf7e90becc6e0ddf6bcaa696d2f4ce4
SAMERkab <samerkab2001@outlook.de>
2021-09-26 16:19:21 +0200

    Make the app easier to use programmatically.
    Make autoupdater.js the new entrypoint because it only exports a class
    and doesn't have any side effects, unlike old index.js.

--- src/index.ts
+++
@@ -1,5 +0,0 @@
-import { Autoupdater } from "./autoupdater";
-
-const projectRoot = process.argv[2] ?? process.cwd();
-const app: Autoupdater = new Autoupdater(projectRoot, process.argv.slice(3));
-app.start();
